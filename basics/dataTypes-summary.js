6cd03fe64b7aae911a659c3a04a754c8acd701eb
Priyanshu Kumar <102993729+AMUL123@users.noreply.github.com>
2025-03-13 09:29:27 +0000

    premitive and non-premitive

---
+++ basics/dataTypes-summary.js
@@ -0,0 +1,10 @@
+// # Primitive
+
+// 7 types: String, Number ,Boolean , Null,Undefined , Symbol,BigInt 
+
+
+//JavaScript is a dynamically typed language, meaning the type of a variable is determined at runtime, not during compilation, and you don't need to explicitly declare the type of a variable. 
+
+//#Reference (Non-Premitive)
+
+//Array, Objects, Functions 
