fbf3565db1d8774a3254b63812450cede3e401e0
Priyanshu Kumar <102993729+AMUL123@users.noreply.github.com>
2025-03-13 16:49:03 +0000

    arrays

---
+++ basics02/arrays.js
@@ -0,0 +1,22 @@
+// arrays
+
+const myArr = [0,1,2,3,4,5]
+
+console.log(myArr[3]);
+
+//Array Methods
+
+// myArr.pop()
+
+
+// console.log(myArr);
+
+//slice , splice
+
+console.log("A", myArr);
+
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+console.log("B", myArr);
+const myn2=myArr.splice(1,3)
+console.log(myn2);
