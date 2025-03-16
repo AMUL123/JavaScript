36a04e6d7413a8b9d4d4434fea17eaf8c22ecae4
Priyanshu Kumar <102993729+AMUL123@users.noreply.github.com>
2025-03-16 05:41:35 +0000

    scope

---
+++ basics03/scodes.js
@@ -0,0 +1,11 @@
+//var c=300 // what ever we declear inside {} is called global scode 
+
+if(true){ // what ever we declear inside {} is called blocked scode 
+    let a = 10;
+    const b =20
+    //var c=30;
+}
+//console.log(a); // this will not print
+//console.log(b);
+ // this will not print
+//console.log(c);// this will print because var can use out of the scope because var previously decleared but it will override 
