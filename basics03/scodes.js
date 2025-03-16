8077402ea5f2db518cc011ea7e83a8d08cd43e2b
Priyanshu Kumar <102993729+AMUL123@users.noreply.github.com>
2025-03-16 06:03:58 +0000

    scope function

--- basics03/scodes.js
+++ basics03/scodes.js
@@ -9,3 +9,34 @@ if(true){ // what ever we declear inside {} is called blocked scode
 //console.log(b);
  // this will not print
 //console.log(c);// this will print because var can use out of the scope because var previously decleared but it will override 
+
+function one(){
+    const username = "samm"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+;    }
+two()
+}
+//one()
+
+if(true){
+    const username="sam"
+    if(username==="sam"){
+        const website = " youtube";
+        //console.log(username + website);
+    }
+}
+
+// +++++++++++++++++++++++++++ interesting +++++++++++++++
+
+function addone(num){ //this is called function
+return num + 1
+}
+//console.log(addone(5)) this can be used before delaration
+
+const addTwo = function(num){ //sometime this is called expresion
+    return num+2;
+}
+//console.log(addTwo(5)) // this can't be used before declaration
