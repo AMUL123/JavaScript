fbd02f895d6bf65f2bf6e9aabae74f880cb52cb4
Priyanshu Kumar <102993729+AMUL123@users.noreply.github.com>
2025-03-17 10:35:55 +0000

    Switch case

---
+++ control_flow/Switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march";
+switch (month) {
+    case "january":
+        console.log("january")
+        break;
+    case "Feb":
+        console.log("Feb")
+        break;
+    case "march":
+        console.log("March")
+        break;
+    case "April":
+        console.log("April")
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
