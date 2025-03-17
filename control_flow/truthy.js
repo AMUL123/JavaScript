fecc2737e41f0ec9bcc0c891e35b311acaf22c89
Priyanshu Kumar <102993729+AMUL123@users.noreply.github.com>
2025-03-17 10:44:01 +0000

    truthy

---
+++ control_flow/truthy.js
@@ -0,0 +1,25 @@
+const userEmail = [];
+
+// if (userEmail) {
+//    console.log("Got user email"); 
+// }else {
+//     console.log("Don't have user email");
+// }
+
+//falsy values
+
+// false, 0,-0, BigInt 0n, "", null, undefined, NaN
+
+//Truthy value
+//"0",'false'," ",[],{},function(){}
+// if (userEmail.length == 0) {
+//  console.log("Array") ;  
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length===0) {
+    console.log("empty")
+}
+
+
